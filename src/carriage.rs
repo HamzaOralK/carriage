306480d325c6d18da8a7a37513d90a3103c3893f
okhuz <haoralk@gmail.com>
2020-08-06 12:04:54 +0300

    URL parser first simple iteration.

--- src/carriage.rs
+++ src/carriage.rs
@@ -53,7 +53,7 @@ impl Carriage {
         let request = String::from_utf8_lossy(&buffer[..]);
         let method = self.get_method(Some(&request[..]));
         let url = self.get_url(Some(&request[..]));
-        println!("{:?}, {:?}", method, url);
+        println!("{:?}, {:?}", method.unwrap(), url.unwrap());
 
         let contents = fs::read_to_string("hello.html").unwrap();
 
@@ -67,7 +67,7 @@ impl Carriage {
         stream.flush().unwrap();
     }
 
-    fn get_method<'a>(&self, request: Option<&'a str>) -> Result<&'a str, &str> {
+    fn get_method<'a>(&self, request: Option<&'a str>) -> Result<String, String> {
         match request {
             Some(request) => {
                 let x = &request.find(" ");
@@ -75,26 +75,25 @@ impl Carriage {
                     Some(x) => &request[0..*x],
                     None => { "" }
                 };
-                return Ok(slice);
+                return Ok(slice.to_string());
             },
             None => {
-                Err("No request")
+                Err("No request".to_string())
             }
         }
     }
 
-    fn get_url<'a>(&self, request: Option<&'a str>) -> Result<&'a str, &str> {
+    fn get_url<'a>(&self, request: Option<&'a str>) -> Result<String, String> {
         match request {
             Some(request) => {
-                let first_result = &request.find(" ").unwrap_or(0);
-                let sliced_first = &request[*first_result..];
+                let first_result = request.find(" ").unwrap_or(0);
+                let sliced_first = &request[first_result+1..];
                 let second_result = sliced_first.find(" ").unwrap_or(0);
-                
-                let final_slice = &request[*first_result..second_result];
-                return Ok(final_slice);
+                let final_slice = &sliced_first[..second_result];
+                return Ok(final_slice.to_string());
             },
             None => {
-                Err("No request")
+                Err("No request".to_string())
             }
         }
     }
