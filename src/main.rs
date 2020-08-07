f880ba7fd77554fdc6c6dccc057c20def2b928ec
okhuz <haoralk@gmail.com>
2020-08-07 10:57:30 +0300

    added second route for test.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ use carriage::method::Method;
 fn main() {
     let mut cr = carriage::Carriage::new("127.0.0.1", "7878", Router::new("my route"));
     let test_route = Route::new(Method::GET, "/users", test);
+    let second_test_route = Route::new(Method::GET, "/products", test2);
     cr.router.add_route(test_route);
+    cr.router.add_route(second_test_route);
 
     cr.connect();
 
@@ -21,3 +23,8 @@ fn test(req: Request) -> Response<'static> {
     res
 }
 
+fn test2(req: Request) -> Response<'static> {
+    println!("{:?}", req);
+    let res = Response { code: "200", body: "{\"test\": 123444}"};
+    res
+}
